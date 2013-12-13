8283cae9fcf96028429f71e3e7e032aac3dc8cc7
percy <gsaico@icloud.com>
2013-12-13 10:02:21 -0500

    Ejercicio 1 completado.

--- src/com/Practica_1/Ejercicio_1.java
+++ src/com/Practica_1/Ejercicio_1.java
@@ -6,18 +6,40 @@ public class Ejercicio_1 {
 
 	public static void main(String[] args) {
 
+		
 		Scanner reader1 = new Scanner(System.in);
-		System.out.println("Ingrese el Numero: ");
-		double v_numerox = reader1.nextDouble();
+	
+		System.out.println(".:   ÁREA DE UN CÍRCULO   :.");
+		String v_numerox;
 
-					
-		double v_resultado = Math.PI * Math.pow(v_numerox , 2);
+		do
+		{
+			System.out.println("Ingrese el Radio del circulo: " );
+			v_numerox = reader1.next();
+		}
+		while ( isNumeric(v_numerox));
+		
+							
+		double v_resultado = Math.PI * Math.pow(Double.parseDouble(v_numerox) , 2);
 
-		System.out.println("El resultado es: " + v_resultado);
+		System.out.println("El ÁREA es igual A = " + v_resultado);
 		
 	
 
 
 	}
-
+	public static boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    double d = Double.parseDouble(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return true;  
+	  }  
+	  return false; 
+	  
+	  
+	}
 }
--- src/com/Practica_1/Ejercicio_2.java
+++ src/com/Practica_1/Ejercicio_2.java
@@ -1,10 +1,46 @@
 package com.Practica_1;
 
+import java.util.Scanner;
+
 public class Ejercicio_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner reader1 = new Scanner(System.in);
+		System.out.println("Ingrese Un Numero: ");
+		String v_numerox = reader1.next();
 
+		do
+		{
+			System.out.println("Por favor ingrese un numero:" );
+			v_numerox = reader1.next();
+		}
+		while ( isNumeric(v_numerox));
+	
+		
+		
+		
 	}
 
+	
+	
+	
+	
+	public static boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    double d = Double.parseDouble(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return true;  
+	  }  
+	  return false; 
+	  
+	  
+	}
+	
+	
+	
+	
 }
